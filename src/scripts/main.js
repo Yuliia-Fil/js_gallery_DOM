eb8a067026cfd76ca68d8298aaac51f065ad1cd1
Yulia Fil <filyulia.yf@gmail.com>
2025-05-03 19:33:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const ul = document.querySelector('ul');
+
+ul.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const bigImage = document.querySelector('#largeImg');
+
+  bigImage.src = e.target.parentElement.href;
+});
